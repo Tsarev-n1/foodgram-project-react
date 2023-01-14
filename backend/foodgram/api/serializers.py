92b07947eb667224087757afb500f827b49ecdc6
Михаил Царев <caruov@yandex.ru>
2023-01-14 14:38:18 +0300

    fixes

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
+
 from users.serializers import UserSerializer
 
 from .models import Ingredient, Recipe, RecipeIngredient, Tag
