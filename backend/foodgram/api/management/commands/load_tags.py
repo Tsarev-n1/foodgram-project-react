0d794cb5e321209319be02c1becf16a310e90742
Михаил Царев <caruov@yandex.ru>
2023-01-15 13:50:53 +0300

    fixes

--- backend/foodgram/api/management/commands/load_tags.py
+++ backend/foodgram/api/management/commands/load_tags.py
@@ -8,8 +8,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         data = [
-            {'name': 'Завтрак', 'color': '#E26C2D', 'slug': 'breakfast'},
-            {'name': 'Обед', 'color': '#49B64E', 'slug': 'lunch'},
-            {'name': 'Ужин', 'color': '#8775D2', 'slug': 'dinner'}]
+            {'name': 'Завтрак', 'hex_code': '#E26C2D', 'slug': 'breakfast'},
+            {'name': 'Обед', 'hex_code': '#49B64E', 'slug': 'lunch'},
+            {'name': 'Ужин', 'hex_code': '#8775D2', 'slug': 'dinner'}]
         Tag.objects.bulk_create(Tag(**tag) for tag in data)
         self.stdout.write(self.style.SUCCESS('Все теги загружены!'))
