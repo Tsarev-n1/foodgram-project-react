a794957effe69960923a9e8b1470c53b823dcdb6
Михаил Царев <caruov@yandex.ru>
2023-03-18 22:28:21 +0300

    fixes

--- backend/foodgram/users/mixins.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.mixins import CreateModelMixin, ListModelMixin
-from rest_framework.viewsets import GenericViewSet
-
-
-class UserMixin(GenericViewSet, ListModelMixin, CreateModelMixin):
-    pass
