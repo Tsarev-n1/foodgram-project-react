365c11ba04f58b79bb9de4bd384c6b9b02b1e2aa
Михаил Царев <caruov@yandex.ru>
2023-03-19 13:56:41 +0300

    fixes

--- backend/foodgram/users/serializers.py
+++ backend/foodgram/users/serializers.py
@@ -34,7 +34,7 @@ class UserSerializer(UserSerializer):
         user = self.context.get('request').user
         if user.is_anonymous:
             return False
-        return Follow.objects.filter(user=user, author=obj.id).exists()
+        return Follow.objects.filter(user=user.id, author=obj.id).exists()
 
 
 class FollowSerializer(UserSerializer):
